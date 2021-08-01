90225107489e145836f2ee11d7ea1c93fd36bd12
Matteo Mazzarolo <me@mazzarolomatteo.com>
2021-08-01 17:18:45 +0200

    Update spotify.config.js

--- packages/scraper/src/company-configs/spotify.config.js
+++ packages/scraper/src/company-configs/spotify.config.js
@@ -1,30 +1,26 @@
-function extractTitle(job) {
-  return (
-    job.title.substr(0, job.title.indexOf("(")).trim() || job.title.trim()
-  ).replace('"', "");
-}
+const fetch = require("node-fetch");
 
-function extractLocation(job) {
-  const regexp = /\(([^)]+)\)/;
-  const regexpResult = regexp.exec(job.title);
-  const location =
-    regexpResult && regexpResult[1] ? regexpResult[1] : job.location;
-  return location.replace(/Remote Eligible/gi, "Remote").trim();
-}
-
-// Spotify job offers have the location between parentheses in the title.
-// Also, sometimes they're wrapped in double quotes.
-// E.g.:
-// - Senior Data Scientist (Remote Eligible - Americas)
-// - Open Source Tech Lead - Platform “(Remote Eligible - Americas)”
-function applyPostScrapingCustomizations(job) {
-  return {
-    ...job,
-    title: extractTitle(job),
-    location: extractLocation(job),
-  };
+async function scrapeJobs() {
+  const websiteJobsRaw = await fetch(
+    "https://api-dot-new-spotifyjobs-com.nw.r.appspot.com/wp-json/animal/v1/job/search"
+  );
+  const { result: websiteJobs } = await websiteJobsRaw.json();
+  const data = websiteJobs
+    .filter((websiteJob) => websiteJob.is_remote)
+    .map((websiteJob) => {
+      return {
+        department: websiteJob.main_category.name,
+        url: `https://www.spotifyjobs.com/jobs/${websiteJob.id}`,
+        title: websiteJob.text,
+        location: websiteJob.remote_name && websiteJob.remote_name.name,
+        id: `https://www.spotifyjobs.com/jobs/${websiteJob.id}`,
+        _id: websiteJob.id,
+        _updatedAt: Date.now(),
+      };
+    });
+  return data;
 }
 
 module.exports = {
-  applyPostScrapingCustomizations,
+  scrapeJobs,
 };
